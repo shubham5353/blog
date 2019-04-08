2a885bd9ced1da42fe54a63a8ad8c13153e9f325
shubham <shubh53153@gmail.com>
2019-04-08 16:50:24 +0530

    Add Ability to Create Articles From Browser

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,4 +1,25 @@
 class ArticleController < ApplicationController
   def index
+  	 @article = Article.new
   end
+
+  def show
+  @article = Article.find(params[:id])
+ end
+
+ def create
+  @article = Article.new(article_params)
+  if @article.save
+   flash[:notice] = "Article was successfully created"
+   redirect_to article_path(@article)
+  else
+   render 'new'
+  end
+ end
+
+private
+  def article_params
+   params.require(:article).permit(:title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get '/article',  to:'article#index'
+  get '/article/:id',  to:'article#show', as: 'article_show'
 
   get '/page', to: 'page#index', as: 'page'
 
   get '/about', to: 'page#about'
+
+  resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
